d53655a7bdbcf79f0079d340ccc764afef1e4a62
oldskool123 <59705400+oldskool123@users.noreply.github.com>
2021-07-02 20:00:14 +0200

    Setup Commerce.js
    - Setup connection to Commerce.js
    - Added Navbar
    - Fetch products from API [Feature]
    - Cart handler [Feature]
    - Removed dummy products

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -11,14 +11,14 @@ import { AddShoppingCart } from '@material-ui/icons';
 
 import useStyles from './styles';
 
-const Product = ({ product }) => {
+const Product = ({ product, onAddToCart }) => {
 	const classes = useStyles();
 
 	return (
 		<Card className={classes.root}>
 			<CardMedia
 				className={classes.media}
-				image={product.image}
+				image={product.media.source}
 				title={product.name}
 			/>
 			<CardContent>
@@ -26,14 +26,21 @@ const Product = ({ product }) => {
 					<Typography variant="h5" gutterBottom>
 						{product.name}
 					</Typography>
-					<Typography variant="h5">{product.price}</Typography>
+					<Typography variant="h5">
+						{product.price.formatted_with_symbol}
+					</Typography>
 				</div>
-				<Typography variant="body2" color="textSecondary">
-					{product.description}
-				</Typography>
+				<Typography
+					dangerouslySetInnerHTML={{ __html: product.description }}
+					variant="body2"
+					color="textSecondary"
+				/>
 			</CardContent>
 			<CardActions className={classes.cardActions} disableSpacing>
-				<IconButton aria-label="Lägg i kundvagn">
+				<IconButton
+					aria-label="Lägg i kundvagn"
+					onClick={() => onAddToCart(product.id, 1)}
+				>
 					<AddShoppingCart />
 				</IconButton>
 			</CardActions>
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,49 +4,14 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-	{
-		id: 1,
-		name: 'Valnötsrot',
-		description: 'Mini Skateboard',
-		price: '1,499 SEK',
-		image:
-			'https://cdn.shopify.com/s/files/1/0553/5875/0894/products/Resized_1_720x.png?v=1620379520',
-	},
-	{
-		id: 2,
-		name: 'Aska',
-		description: 'Mini Skateboard',
-		price: '1,499 SEK',
-		image:
-			'https://cdn.shopify.com/s/files/1/0553/5875/0894/products/Untitled1_4a4242a0-dd6c-4ae6-a139-ff9f1f4f7342_1512x.jpg?v=1623225132',
-	},
-	{
-		id: 3,
-		name: 'Rosewood',
-		description: 'Mini Skateboard',
-		price: '1,499 SEK',
-		image:
-			'https://cdn.shopify.com/s/files/1/0553/5875/0894/products/FULFORO_1_720x.png?v=1620379520',
-	},
-	{
-		id: 4,
-		name: 'Bläck',
-		description: 'Mini Skateboard',
-		price: '1,499 SEK',
-		image:
-			'https://cdn.shopify.com/s/files/1/0553/5875/0894/products/froblackJPG_9ce83035-3099-4f67-b0e1-64513e7893e7_720x.jpg?v=1624878656',
-	},
-];
-
-const Products = () => {
+const Products = ({ products, onAddToCart }) => {
 	const classes = useStyles();
 	return (
 		<main className={classes.content}>
 			<Grid className={classes.toolbar} container justify="center" spacing={4}>
 				{products.map((product) => (
 					<Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-						<Product product={product} />
+						<Product product={product} onAddToCart={onAddToCart} />
 					</Grid>
 				))}
 			</Grid>
